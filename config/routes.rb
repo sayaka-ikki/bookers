c2b553be4102fbad436ca5f550c78473f52fede5
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-03-28 15:45:06 +0000

    bookers3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get "/books" => 'books#index'
-  get 'books/show'
-  get "/books/edit" => 'books#edit'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end

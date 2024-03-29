2232c13ce5097897b99bd0e27e6fcec355a3b418
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-03-29 13:07:44 +0000

    bookers-6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,8 +26,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
